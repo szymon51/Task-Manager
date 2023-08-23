b617c85cdf9814bd3c64b2c13bc3fe034955a415
Szymon51 <120176777+szymon51@users.noreply.github.com>
2023-08-23 14:41:39 +0200

    Add the finished project

---
+++ app.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const app = express();
+const tasks = require('./routes/tasks');
+const connectDB = require('./db/connect');
+require('dotenv').config();
+const notFound = require('./middleware/not-found');
+const errorHandlerMiddleware = require('./middleware/error-handler');
+
+app.use(express.json());
+app.use(express.static('./public'));
+
+app.get('/', (req, res) => {
+  res.status(200).send('Task manager app');
+});
+
+app.use('/api/v1/tasks', tasks);
+
+app.use(notFound);
+app.use(errorHandlerMiddleware);
+
+const port = process.env.PORT || 3000;
+
+const start = async () => {
+  try {
+    await connectDB(process.env.MONGO_URI);
+    app.listen(port, console.log(`server is running on port ${port}`));
+  } catch (error) {
+    console.log(error);
+  }
+};
+start();
